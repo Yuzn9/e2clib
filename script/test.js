c066f2314332804296c2b0d500f889b65d4fce73
Yuzn9 <th.mlv17@gmail.com>
2025-12-01 16:38:35 +0100

    Début du JS

---
+++ script/test.js
@@ -0,0 +1,28 @@
+/* 
+let username = "Yuzz"
+let number = 25 // integer
+let double = 2.5 // double 
+let boolean = true // ou false
+
+console.log(username, number, double, boolean);
+
+let number1 = "salut"
+let number2 = " coucou" 
+
+console.log(number1 + number2)
+console.log(number1 - number2)
+console.log(number1 * number2)
+console.log(number1 / number2)
+console.log(number1 % number2)
+
+username = "Azuy"
+console.log (username) 
+*/
+
+let firstname;
+let lastname;
+
+firstname = prompt("Quel est votre prénom ?");
+lastname = prompt("Quel est votre nom de famille ?");
+
+alert(`Bonjour ${firstname} ${lastname}, comment vas-tu ?`); 
