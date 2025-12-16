75e5191354e032db81136fe659781fe2344f969f
Yuzn9 <th.mlv17@gmail.com>
2025-12-16 16:31:40 +0100

    Suite MySQL

--- model/bdd.sql
+++ model/bdd.sql
@@ -32,3 +32,61 @@ INSERT INTO users (password, email, pseudo, age)
 
 UPDATE users 
  SET age=19 WHERE pseudo=Pokaguare;
+
+CREATE TABLE IF NOT EXISTS genres (
+    id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS formats (
+    id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS languages (
+    id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
+INSERT INTO languages (name)
+    VALUES ('Français'),('Anglais'),('Allemand'),('Arabe');
+
+INSERT INTO formats (name)
+    VALUES ('Poche'),('Roman'),('Manga'),('BD'),('Magazine');
+
+INSERT INTO genres (name)
+    VALUES ('Non classé'),('Policier'),('Drame'),('Action'),('Science-fiction'),('Thriller'),('Fantaisie'),('Bande dessinée'),('Educatif'),('Manga'),('Politique');
+
+CREATE TABLE IF NOT EXISTS books (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL, 
+    auteur VARCHAR(255) NOT NULL DEFAULT 'Inconnu',
+    synopsis TINYTEXT NOT NULL,
+    year SMALLINT,
+    editor VARCHAR(255),
+    cover VARCHAR(255),
+    pages SMALLINT,
+    genres_id TINYINT UNSIGNED NOT NULL DEFAULT 1, 
+    languages_id TINYINT UNSIGNED NOT NULL DEFAULT 1,
+    formats_id TINYINT UNSIGNED,
+    CONSTRAINT fk_genres
+        FOREIGN KEY (genres_id)
+        REFERENCES genres(id),
+    CONSTRAINT fk_languages 
+        FOREIGN KEY (languages_id)
+        REFERENCES languages(id),
+    CONSTRAINT fk_formats 
+        FOREIGN KEY (formats_id)
+        REFERENCES formats(id)
+)ENGINE=InnoDB;
+
+SELECT books.title AS Titre, books.auteur AS Auteur, books.pages AS 'Nombre de pages', genres.name AS Genre, formats.name AS Format, languages.name AS Langue FROM books
+INNER JOIN genres ON books.genres_id = genres.id
+INNER JOIN formats ON books.formats_id = formats.id
+INNER JOIN languages ON books.languages_id = languages.id;
+
+/* faire une vue */
+CREATE VIEW books_vw AS (SELECT books.title AS Titre, books.auteur AS Auteur, books.pages AS 'Nombre de pages', genres.name AS Genre, formats.name AS Format, languages.name AS Langue FROM books
+INNER JOIN genres ON books.genres_id = genres.id
+INNER JOIN formats ON books.formats_id = formats.id
+INNER JOIN languages ON books.languages_id = languages.id);
